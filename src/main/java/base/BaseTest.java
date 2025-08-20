cd3c3f5d647f95fe808bc8cb4566833404ee81e9
Mike Redondo <redondomikej@gmail.com>
2025-08-20 09:15:37 +0800

    update files

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -13,7 +13,8 @@ public class BaseTest {
     public void startBrowser(String browserType, boolean headless, String url){
         switch (browserType.toLowerCase()){
             case "chrome" -> {
-                WebDriverManager.chromedriver().setup();
+//                WebDriverManager.chromedriver().setup();
+                System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
                 ChromeOptions chromeOptions = new ChromeOptions();
                 // Always add Docker-friendly flags
                 chromeOptions.addArguments("--no-sandbox");
